2bfa0295312120e885ee0cb52891bdd4cba79f27
Totto16 <tobiausgais@gmail.com>
2024-06-22 22:03:14 +0200

    add example prorgam, that uses this library

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
 module Main (main) where
 
-import Lib ()
+import Lib (RecordingReturnValue (RecordingReturnError, RecordingReturnSuccess), getRecordingInformation, isRecordingFile)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  putStrLn "TEST"
+  args <- getArgs
+  if length args /= 1
+    then do
+      putStrLn "Usage: <file>"
+    else do
+      let file = head args
+      isRecFile <- isRecordingFile file
+      if not isRecFile
+        then do
+          putStrLn "Input file is no recordings file!"
+        else do
+          returnValue <- getRecordingInformation file
+          case returnValue of
+            RecordingReturnError err -> putStrLn $ "An error occurred: " ++ err
+            RecordingReturnSuccess information -> print information
+          return ()
